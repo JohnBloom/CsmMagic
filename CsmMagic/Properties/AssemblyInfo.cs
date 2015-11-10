9cf32357d61f029abf122294c3961c8e0dc7cc8f
John Bloom <john.bloom@cleardata.com>
2015-11-09 22:38:43 -0800

    updating to v1

--- CsmMagic/Properties/AssemblyInfo.cs
+++ CsmMagic/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("a63596f2-835b-447a-af26-32d838669eb7")]
 
-[assembly: AssemblyInformationalVersion("0.9.8.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 
-[assembly: AssemblyVersion("0.9.8.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
-[assembly: AssemblyFileVersion("0.9.8.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
